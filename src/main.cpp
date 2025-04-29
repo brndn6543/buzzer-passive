7bf909083a2231b2ec3f2ff3f6b15c62c7590650
brndn6543 <brndn.6543@gmail.com>
2025-04-29 15:20:56 -0400

    Updated comments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <pitches.h>
 
+// Scale tones are defined in pitches.h.
 int scale[] = {NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
 int duration = 500;
 
@@ -8,6 +9,9 @@ void setup() {}
 
 void loop() {
     for (int note = 0; note < sizeof(scale); note++) {
+        // tone() is standard with the Arduino API.
+        // It allows the generations of a square-wave
+        // of a specified frequency.
         tone(8, scale[note], duration);
         delay(1000);
     }
